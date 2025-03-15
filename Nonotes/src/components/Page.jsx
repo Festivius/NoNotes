9be8c1f44d9f48e8b4c41b7bbf71f9f0b48485d3
Festivius <pineapplealien101@gmail.com>
2025-03-15 13:52:42 -0700

    more features

--- Nonotes/src/components/Page.jsx
+++ Nonotes/src/components/Page.jsx
@@ -4,35 +4,107 @@ import Toolbar from "./Toolbar";
 
 const Page = () => {
   const emojis = ['😊', '😂', '😍', '😎', '🥺', '🙌', '👍', '❤️'];
+  const commonMisspelledWords = [
+    "teh", "waht", "liek", "hwen", "definately", "seperate", "becase", "thier", "freind", "adress", "beleive", 
+    "tommorow", "recieve", "occured", "occassionaly", "independant", "embarassment", 
+    "calender", "seperete", "alot", "noticable", "resturant", "preferance", "ocasian", 
+    "wierd", "necassary", "defiantly", "accomodate", "achieve", "a lot", "believe", 
+    "calendar", "colonel", "definitely", "embarrassment", "exceed", "independent", 
+    "necessary", "occasion", "receive", "separate", "recommend", "referred", "restaurant", 
+    "schedule", "supercede", "tomorrow", "occasionally", "privilege", "vacuum", "mischievous", 
+    "independent", "misspell", "occurence", "arguement", "recognize", "refered", "publically", 
+    "occurred", "awhile", "untill", "seperately", "maintainance", "reccommend", "beleive", 
+    "tommorow", "embarassed", "technoloy", "continuos", "millenium", "yatch", "definately", 
+    "untill", "alright", "interupt", "wierd", "maneuver", "preceed", "decent", "realy", "wich", 
+    "embarassing", "parliment", "favour", "color", "honor", "theater", "travell", "prefered", 
+    "tomatoe", "serch", "supersede", "truley", "sux", "untill", "thorough", "begining", "occurred", 
+    "outfit", "recieve", "overexaggerate", "unecessary", "recomend", "publically", "sorrry", 
+    "defintely", "diffferent", "congradulations", "alot", "succeed", "similiar", "appologies", 
+    "believe", "conscience", "parliment", "guarrantee"
+  ];
+  const correctSpellings = [
+    "the", "what", "like", "when", "definitely", "separate", "because", "their", "friend", "address", "believe", 
+    "tomorrow", "receive", "occurred", "occasionally", "independent", "embarrassment", 
+    "calendar", "separate", "a lot", "noticeable", "restaurant", "preference", "occasion", 
+    "weird", "necessary", "definitely", "accommodate", "achieve", "a lot", "believe", 
+    "calendar", "colonel", "definitely", "embarrassment", "exceed", "independent", 
+    "necessary", "occasion", "receive", "separate", "recommend", "referred", "restaurant", 
+    "schedule", "supercede", "tomorrow", "occasionally", "privilege", "vacuum", "mischievous", 
+    "independent", "misspell", "occurrence", "argument", "recognize", "referred", "publicly", 
+    "occurred", "a while", "until", "separately", "maintenance", "recommend", "believe", 
+    "tomorrow", "embarrassed", "technology", "continuous", "millennium", "yacht", "definitely", 
+    "until", "alright", "interrupt", "weird", "maneuver", "precede", "decent", "really", "which", 
+    "embarrassing", "parliament", "favor", "color", "honor", "theater", "travel", "preferred", 
+    "tomato", "search", "supersede", "truly", "sucks", "until", "thorough", "beginning", "occurred", 
+    "outfit", "receive", "overexaggerate", "unnecessary", "recommend", "publicly", "sorry", 
+    "definitely", "different", "congratulations", "a lot", "succeed", "similar", "apologies", 
+    "believe", "conscience", "parliament", "guarantee"
+  ];  
   const [content, setContent] = useState("");
   const [notes, setNotes] = useState([]);
   const [currentNote, setCurrentNote] = useState({ id: 1, title: "Untitled Note", content: "" });
   const [isSaved, setIsSaved] = useState(true);
 
     const handleContentChange = (e) => {
-    const newContent = e.target.value;
+    var newContent = e.target.value;
     setCurrentNote({ ...currentNote, content: newContent });
     setIsSaved(false);
 
     const punctuationRegex = /[.,;!?(){}\[\]"':-]/;
+    const numberRegex = /[23456789]/;
     let updatedNote = '';
     let currentWord = '';
     let inWord = false;
 
+    for (let i = 0; i < 103; i++) {
+        if (newContent.includes(correctSpellings[i])) {
+            console.log('rahahh"');
+            newContent = newContent.replace(correctSpellings[i], commonMisspelledWords[i]);
+        }
+    }
+
+    if (punctuationRegex.test(newContent.charAt(newContent.length-1)) == false) {
+
     for (let i = 0; i < newContent.length; i++) {
         const char = newContent[i];
+        if (numberRegex.test(char)) {
+            console.log('nummm');
+            let number = char;
+            let binaryRepresentation = "";
+
+            if (number === '2') {
+            binaryRepresentation = "10";
+            } else if (number === '3') {
+            binaryRepresentation = "11";
+            } else if (number === '4') {
+            binaryRepresentation = "100";
+            } else if (number === '5') {
+            binaryRepresentation = "101";
+            } else if (number === '6') {
+            binaryRepresentation = "110";
+            } else if (number === '7') {
+            binaryRepresentation = "111";
+            } else if (number === '8') {
+            binaryRepresentation = "1000";
+            } else if (number === '9') {
+            binaryRepresentation = "1001";
+            }
+
+            updatedNote += binaryRepresentation;
+        }
 
         if (char.match(/\s/) || punctuationRegex.test(char)) {
-            // If we finished a word, append it as lowercase to updatedNote
             if (currentWord) {
-                updatedNote += currentWord.toLowerCase(); // Ensure lowercase is applied here
+                updatedNote += currentWord.toLowerCase();
                 currentWord = '';
             }
-            updatedNote += char; // Add the punctuation or space
+            updatedNote += char;
             inWord = false;
         } else {
-            currentWord += char; // Build the word
-            inWord = true;
+            if (numberRegex.test(char) == false) {
+                currentWord += char;
+                inWord = true;
+            }
         }
     }
 
@@ -55,6 +127,8 @@ const Page = () => {
       setContent(newContent + randomEmoji); 
     }
   };
+
+  }
 
 
   const handleSave = () => {
